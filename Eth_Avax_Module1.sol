c052f0c5cda21e23ebe7e0d15602473b6a5037e7
Aryan1Revolution <onlyformail1234@gmail.com>
2024-06-13 11:59:39 +0530

    Update Eth_Avax_Module1.sol

--- Eth_Avax_Module1.sol
+++ Eth_Avax_Module1.sol
@@ -1,42 +1,84 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
-contract Module1 {
-    address public owner;
-    uint256 public balance;
+contract IronManSuitDevelopment {
+    address public TonyStark;
+    uint256 public suitFund;
+    uint256 public proposalCount;
 
-    constructor() {
-        owner = msg.sender; 
+    struct Proposal {
+        uint256 id;
+        string description;
+        uint256 cost;
+        uint256 votes;
+        bool funded;
+        mapping(address => bool) voters;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the contract owner");
+    mapping(uint256 => Proposal) public proposals;
+    address[] public avengers;
+
+    constructor(address[] memory _avengers) {
+        TonyStark = msg.sender;
+        avengers = _avengers;
+        proposalCount = 0;
+    }
+
+    modifier onlyTonyStark() {
+        require(msg.sender == TonyStark, "You are not Tony Stark");
         _;
     }
 
-    event Deposit(address indexed depositor, uint256 amount);
-
-    function deposit(uint256 amount) public payable {
-        require(msg.value == amount, "Incorrect value sent");
-        balance = balance + amount;
-        emit Deposit(msg.sender, amount);
+    modifier onlyAvengers() {
+        bool isAvenger = false;
+        for (uint256 i = 0; i < avengers.length; i++) {
+            if (msg.sender == avengers[i]) {
+                isAvenger = true;
+                break;
+            }
+        }
+        require(isAvenger, "You are not an Avenger");
+        _;
     }
 
-    function withdraw(uint256 amount) public onlyOwner {
-        require(amount <= balance, "Insufficient balance");
-        balance = balance - amount;
-        payable(msg.sender).transfer(amount);
+    function contributeToSuitFund() public payable {
+        require(msg.value > 0, "You must contribute some ether");
+        suitFund += msg.value;
     }
 
-    function checkBalance(uint256 expectedBalance) public view {
-        assert(balance == expectedBalance);
+    function proposeSuitDevelopment(string memory description, uint256 cost) public onlyAvengers {
+        require(cost > 0, "Development cost must be greater than zero");
+        Proposal storage newProposal = proposals[proposalCount];
+        newProposal.id = proposalCount;
+        newProposal.description = description;
+        newProposal.cost = cost;
+        newProposal.votes = 0;
+        newProposal.funded = false;
+        proposalCount++;
     }
 
-    function emergencyWithdraw() public pure {
-        revert("Emergency withdraw is not allowed");
+    function voteOnProposal(uint256 proposalId) public onlyAvengers {
+        Proposal storage proposal = proposals[proposalId];
+        require(!proposal.voters[msg.sender], "You have already voted on this proposal");
+        proposal.voters[msg.sender] = true;
+        proposal.votes++;
     }
 
-    function getOwner() public view returns (address) {
-        return owner;
+    function fundSuitDevelopment(uint256 proposalId) public onlyTonyStark {
+        Proposal storage proposal = proposals[proposalId];
+        require(proposal.cost <= suitFund, "Insufficient funds to develop the Iron Man suit");
+        require(proposal.votes > avengers.length / 2, "Not enough votes to approve the proposal");
+        require(!proposal.funded, "Proposal has already been funded");
+        suitFund -= proposal.cost;
+        proposal.funded = true;
+    }
+
+    function checkSuitFundBalance() public view returns (uint256) {
+        return suitFund;
+    }
+
+    function emergencyShutdown() public onlyTonyStark {
+        // In case of emergency, Tony Stark can shut down the contract and withdraw remaining funds
+        payable(TonyStark).transfer(address(this).balance);
     }
 }
